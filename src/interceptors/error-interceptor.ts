67c250db1603c85361a2ce22ba93abbd96e03a69
edsonbetke <edsonb@gmail.com>
2021-10-22 10:54:52 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,3 +1,4 @@
+import { StorageService } from "./../services/storage.service";
 import {
   HttpEvent,
   HttpHandler,
@@ -10,6 +11,8 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(public storage: StorageService) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
@@ -28,8 +31,18 @@ export class ErrorInterceptor implements HttpInterceptor {
       console.log("Erro detectado pelo interceptor: ");
       console.log(errorObj);
 
+      switch (errorObj.status) {
+        case 403:
+          this.handle403();
+          break;
+      }
+
       return Observable.throw(errorObj);
     }) as any;
+  }
+
+  handle403() {
+    this.storage.setLocalUser(null);
   }
 }
 
