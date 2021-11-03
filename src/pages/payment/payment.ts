4eca37c2461ca1213b5ac20362756919f461139c
edsonbetke <edsonb@gmail.com>
2021-11-03 10:05:42 -0300

    Tela de escolha de pagamento

---
+++ src/pages/payment/payment.ts
@@ -0,0 +1,43 @@
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { PedidoDTO } from "./../../models/pedido.dto";
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+
+/**
+ * Generated class for the PaymentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: "page-payment",
+  templateUrl: "payment.html",
+})
+export class PaymentPage {
+  //variável que recebe o pedido da página de escolha de endereco
+  pedido: PedidoDTO;
+
+  parcelas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  formGroup: FormGroup;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+    //pega o pedido da pagina de escolha de endereco
+    this.pedido = this.navParams.get("pedido");
+
+    this.formGroup = this.formBuilder.group({
+      numeroDeParcelas: [1, Validators.required],
+      "@type": ["pagamentoComCartao", Validators.required],
+    });
+  }
+
+  nextPage() {
+    this.pedido.pagamento = this.formGroup.value;
+    console.log(this.pedido);
+  }
+}
