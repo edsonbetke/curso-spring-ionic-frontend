e3171fbba17c148c6b21e3db428a9419b721f7b2
edsonbetke <edsonb@gmail.com>
2021-12-06 10:48:19 -0300

    Bugfix - cancelamento da camera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -77,7 +77,7 @@ export class ProfilePage {
         this.cameraOn = false;
       },
       (err) => {
-        // Handle error
+        this.cameraOn = false;
       }
     );
   }
@@ -99,7 +99,7 @@ export class ProfilePage {
         this.cameraOn = false;
       },
       (err) => {
-        // Handle error
+        this.cameraOn = false;
       }
     );
   }
